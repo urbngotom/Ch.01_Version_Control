05f1f5f1705b7d4f58909da7758ea02d03f492b1
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-09 12:49:27 -0500

    Update 1.0_Jedi_Training.py
    combined

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,52 @@ Sign your name:________________
 
 
 
-Define Forking: 
+1. Define Forking: 
 
-Define Cloning: 
+2. Define Cloning: 
 
-Define Branching:
+3. Define Branching:
 
-Define Committing: 
+4. Define Committing: 
 
-Define Merging: 
+5. Define Merging: 
 
-Define Pushing:
+6. Define Pushing:
 
-Define Pull Request:
+7. Define Pull Request:
 
 
+8. TURTORIAL ART
+
+Modify the starter code below to create your own cool drawing. 
+Make sure you keep the last two lines of code!!!! 
+Your first and last name must be written on your art.
+The last line keeps the window open until you click to close.
+Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
+import turtle
+yoda=turtle.Turtle()
+screen=turtle.Screen() # makes a screen object
+screen.bgcolor('black') # colors the screen
+yoda.pensize(3) # width of pen line
+yoda.speed(10)  # speed of drawing. Go fast to not waste time.
+yoda.color("#00FF00")
+yoda.circle(100)  #head
+yoda.penup()
+yoda.setpos(50,185) #right ear
+yoda.pendown()
+yoda.goto(200,210)
+yoda.goto(88,145)
+yoda.penup()
+yoda.setpos(-50,185)  #left ear
+yoda.pendown()
+yoda.goto(-200,210)
+yoda.goto(-88,145)
+yoda.penup()
+yoda.setpos(200,-300)
+yoda.pendown()
+yoda.pencolor('#00FF00')
+
+
+yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
